84e12f30b5c22138fd2ebf52fd687ec94bcba563
jessicalc <jessicalaurenchong@gmail.com>
2015-08-29 16:49:10 -0700

    Added happiness levels and replaced svg shapes with images

--- client/assets/index_files/watchout.js
+++ client/assets/index_files/watchout.js
@@ -3,7 +3,7 @@
 var gameOptions = {
   height: 600,
   width: 800,
-  enemies: 20,
+  enemies: 15,
   padding: 20,
   enemyRadius: 15
 };
@@ -12,8 +12,7 @@ var gameStats = {
   score: 0,
   bestScore: 0,
   collisions: 0,
-  collided: false,
-  counter: 0
+  collided: false
 };
 
 var axes = {
@@ -36,11 +35,8 @@ var updateCollisionCount = function() {
 };
 
 var updateBestScore = function() {
-  var prev = gameStats.bestScore;
   gameStats.bestScore = Math.max(gameStats.bestScore, gameStats.score);
-  if (prev !== gameStats.bestScore) {
-    d3.select(".high").transition().duration(250).style("background-color", "red").text("High score: " + gameStats.bestScore.toString()).transition().duration(500).style("background-color", "");
-  }
+  d3.select(".high").transition().duration(500).style("background-color", "yellow").text("High score: " + gameStats.bestScore.toString()).transition().duration(500).style("background-color", "");
 };
 
 var createEnemies = function() {
@@ -58,7 +54,6 @@ var Player = function() {
   this.x = gameOptions.width/2;
   this.y = gameOptions.height/2;
   this.color = "red";
-  this.happinessLevel = 0;
 };
 
 var drag = d3.behavior.drag()
@@ -66,8 +61,6 @@ var drag = d3.behavior.drag()
 
 function dragmove(d) {
   var playerRadius = player.data()[0].r;
-  gameStats.counter = 0;
-
   d3.select(this)
       .attr("x", d.x = Math.max(playerRadius, Math.min(gameOptions.width - playerRadius, d3.event.x)) - playerRadius)
       .attr("y", d.y = Math.max(playerRadius, Math.min(gameOptions.height - playerRadius, d3.event.y)) - playerRadius);
@@ -84,7 +77,7 @@ var player = d3.select('svg')
   .attr("class", "player")
   .attr("width", "30px")
   .attr("height", "30px")
-  .attr("xlink:href", "assets/" + newPlayer[0].happinessLevel + ".png")
+  .attr("xlink:href", "assets/unamused.png")
   .attr("x", function(d) { return axes.x(50) })
   .attr("y", function(d) { return axes.y(50) })
   .attr("r", function(d) { return d.r })
@@ -152,46 +145,18 @@ var detectCollision = function(item) {
     var sumRadius = playerPos.r + gameOptions.enemyRadius;
 
     if (distance < sumRadius && !gameStats.collided) {
-      var happyLevel = player.data()[0].happinessLevel;
-      if (happyLevel > 0) {
-        happyLevel = 0;
-      }
-
       gameStats.collided = true;
       gameStats.collisions++;
-
-      if (gameStats.collisions === 5) {
-        happyLevel = -1;
-      } else if (gameStats.collisions === 10) {
-        happyLevel = -2;
-      } else if (gameStats.collisions >= 15) {
-        happyLevel = -3;
-      }
-
-      gameStats.counter = 0;
       d3.select("svg").style("background", "#A00").transition().duration(500).style("background", "white");
-      d3.select(".player").transition().duration(500).attr("xlink:href", "assets/dead.png").transition().duration(500).attr("xlink:href", "assets/" + happyLevel + ".png");
       updateBestScore();
       updateCollisionCount();
       gameStats.score = 0;
       console.log("You've been hit!");
-      player.data()[0].happinessLevel = happyLevel;
     }
   }
 }
 
 setInterval(function() {
   gameStats.score++;
-  gameStats.counter++;
-  if (gameStats.score === 100) {
-
-    player.data()[0].happinessLevel = 1;
-    d3.select(".player").attr("xlink:href", "assets/" + player.data()[0].happinessLevel + ".png");
-  }
-
-  if (gameStats.counter > 100) {
-    d3.select(".player").attr("xlink:href", "assets/zzz.png");
-  }
-
   updateScore()
 }, 50);
